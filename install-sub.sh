2d05edc30ac441560f8610876d53969b8437444a
ngocdoan <vuongngocdoan@gmail.com>
2022-09-20 23:04:54 +0700

    Update install-sub.sh

--- install-sub.sh
+++ install-sub.sh
@@ -1,13 +1,11 @@
 #!/bin/bash
-sudo apt install dialog -y
-
 function install-apps {
 	sudo apt update && sudo apt upgrade -y
 	sudo apt install curl build-essential git wget jq make gcc ack tmux ncdu unzip wget git dialog -y
 	sudo wget -qO /usr/local/bin/yq https://github.com/mikefarah/yq/releases/download/v4.27.3/yq_linux_amd64 && chmod +x /usr/local/bin/yq
 	sudo apt update
 }
-
+###########################################################
 function install-docker {
 	curl -fsSL https://get.docker.com -o get-docker.sh
 	sudo sh get-docker.sh
@@ -31,38 +29,9 @@ else
 fi
 }
 #######################################################
-######### Dialog menu ##########################################
-HEIGHT=15
-WIDTH=40
-CHOICE_HEIGHT=4
-BACKTITLE="Owlstake"
-TITLE="Susbspace installer helper"
-MENU="Choose one of the following options:"
-
-OPTIONS=(1 "Installer docker and dependencies"
-         2 "Option 2"
-         3 "Option 3")
-
-CHOICE=$(dialog --clear \
-                --backtitle "$BACKTITLE" \
-                --title "$TITLE" \
-                --menu "$MENU" \
-                $HEIGHT $WIDTH $CHOICE_HEIGHT \
-                "${OPTIONS[@]}" \
-                2>&1 >/dev/tty)
-
-clear
-################## Enter command for each choice ###########
-case $CHOICE in
-        1)
-            echo "You chose Option 1"
-			install-stack
-            ;;
-        2)
-            echo "You chose Option 2"
-            ;;
-        3)
-            echo "You chose Option 3"
-            ;;
-esac
-##############################################################
+install-stack
+cd $HOME
+rm -rf subspace-docker-folder
+mkdir subspace-docker-folder
+cd subspace-docker-folder
+PORTS = (4000 5000 6000)
