46c78f81409b193b44818ddf4dec2fc221d1f602
ngocdoan <vuongngocdoan@gmail.com>
2022-09-30 00:49:04 +0700

    Update install-part1.sh

--- install-part1.sh
+++ install-part1.sh
@@ -1,43 +1,55 @@
 #!/bin/bash
+counter=0
+CPUCORE=$(nproc)
+readarray -t arrrewardaddress <rewardaddress.txt 2> /dev/null
+arrlength=${#arrrewardaddress[@]}
+if [[ arrlength -eq 0 ]]
+then
+        echo "plz add reward address on the file rewardaddress.txt"
+        exit
+fi
+NODEQUANTITY=0
+PORTARRAY=()
 #########################
 #Ask port ranges array
 function ADDPORTARRAY {
-PORTARRAY=()
-echo "Please provide 3 port ranges for the nodes"
-read -p -r 'Port range #1: Press ENTER if you want to use the default port' -e -i '4000' port1
-PORTARRAY+=("$port1")
-read -p -r 'Port range #2: Press ENTER if you want to use the default port' -e -i '5000' port2
-PORTARRAY+=("$port2")
-read -p -r 'Port range #3: Press ENTER if you want to use the default port' -e -i '6000' port3
-PORTARRAY+=("$port3")
+PORTARRAY+=(4000)
+PORTARRAY+=(5000)
+PORTARRAY+=(6000)
 }
 ##########################
-function start-install {
-cd $HOME || exit
-rm -rf subspace-docker-folder
-mkdir subspace-docker-folder
-cd subspace-docker-folder || exit
-mkdir $NODEQUANTITY
-cd $NODEQUANTITY || exit
+function install {
+cd $HOME/subspace-docker-folder
+mkdir $counter
+cd $counter
 wget -O docker-compose.yaml https://raw.githubusercontent.com/owlstake/testnet/main/subspace/docker-compose.yaml
-sudo tee $HOME/$node-number/.env > /dev/null <<EOF
+touch .env
+ls -l
+sudo tee $HOME/subspace-docker-folder/$counter/.env > /dev/null <<EOF
 IMAGETAG=gemini-2a-2022-sep-10
-NODENAME=owlstake$node-number
-PORT1=PORTARRAY[0]
-PORT2=PORTARRAY[1]
-PORT3=PORTARRAY[2]
-REWARDADDRESS=$(arrrewardaddress[$NODEQUANTITY])
+NODENAME=owlstake$counter
+PORT1=${PORTARRAY[0]}
+PORT2=${PORTARRAY[1]}
+PORT3=${PORTARRAY[2]}
+REWARDADDRESS=${arrrewardaddress[$counter]}
 PLOTSIZE=10G
 EOF
 ############
-docker compose up -d
-sleep 10
+PORTARRAY[0]=$((PORTARRAY[0]+1))
+PORTARRAY[1]=$((PORTARRAY[1]+1))
+PORTARRAY[2]=$((PORTARRAY[2]+1))
+#docker compose up -d
+#sleep 10
 }
 ##########################
-CPUCORE=$(nproc)
-readarray -t arrrewardaddress <rewardaddress.txt
-arrlength=${#arrrewardaddress[@]}
-NODEQUANTITY=0
+function start-install {
+while [ $counter -lt $NODEQUANTITY ]
+do
+install
+((counter++))
+done
+}
+##########################
 if [[ $CPUCORE -eq $arrlength ]]
 then
 echo "Number of CPU core is $CPUCORE"
@@ -45,6 +57,10 @@ echo "Number of reward address is $arrlength"
 echo "Result: equal --> Starting to install the node"
 ###############################
 # Start install function here
+ADDPORTARRAY
+rm -rf subspace-docker-folder
+mkdir subspace-docker-folder
+cd subspace-docker-folder
 start-install
 ###############################
 NODEQUANTITY=$CPUCORE
@@ -58,6 +74,10 @@ NODEQUANTITY=$CPUCORE
 echo "We will install $NODEQUANTITY nodes"
 ###############################
 # Start install function here
+ADDPORTARRAY
+rm -rf subspace-docker-folder
+mkdir subspace-docker-folder
+cd subspace-docker-folder
 start-install
 ###############################
 elif [[ $CPUCORE -gt $arrlength ]]
@@ -69,11 +89,11 @@ echo "Result: CPU core > reward address"
 while true; do
 read -p -r "Do you want to continue install the nodes based on number of reward address? (yes/no) " yn
 case $yn in 
-	yes ) echo ok, we will proceed;
-		break;;
-	no ) echo exiting...;
-		exit;;
-	* ) echo invalid response;;
+        yes ) echo ok, we will proceed;
+                break;;
+        no ) echo exiting...;
+                exit;;
+        * ) echo invalid response;;
 esac
 done
 ###############################
@@ -81,8 +101,11 @@ NODEQUANTITY=$arrlength
 echo "We will install $NODEQUANTITY nodes"
 ###############################
 # Start install function here
+ADDPORTARRAY
+rm -rf subspace-docker-folder
+mkdir subspace-docker-folder
+cd subspace-docker-folder
 start-install
 ###############################
 #####
 fi
-
